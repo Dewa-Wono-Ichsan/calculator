42c516de6e5631531c156076853d9b5a83bf1d7a
Dewa Wono Ichsan <dewa.wono.ichsan@gmail.com>
2024-04-29 22:21:49 +0700

    Refacture function in decimal sign button event listener
    Give display result with decimal sign of first number and second number of display result when user click decimal sign button.
    
    Prevent user to input more than one decimal sign in first number and second number of display result. If number have more than one decimal sign, cause issue to calculate number between value of variable numberOne and variable numberTwo.

--- script.js
+++ script.js
@@ -229,7 +229,8 @@ decimal.addEventListener('click', function() {
 
         if (result.textContent.includes('+') === false
             && result.textContent.includes('-') === false
-            && result.textContent.includes('x') === false) {
+            && result.textContent.includes('x') === false
+            && result.textContent.includes('/') === false) {
         
             if(result.textContent === '') {
 
@@ -281,6 +282,21 @@ decimal.addEventListener('click', function() {
                 result.textContent += ` ${0 + this.textContent}`
             }
             else if (result.textContent.slice(result.textContent.indexOf('x') + 1).includes('.') === false) {
+                
+                result.textContent += this.textContent
+            }
+            else {
+
+            result.textContent
+            }
+        }
+        else if (result.textContent.includes('/') === true) {
+
+            if (result.textContent.charAt(result.textContent.indexOf('/') + 2) === '') {
+
+                result.textContent += ` ${0 + this.textContent}`
+            }
+            else if (result.textContent.slice(result.textContent.indexOf('/') + 1).includes('.') === false) {
                 
                 result.textContent += this.textContent
             }
